869644359bff6a3d4d159507d892d698df5745e7
Ayça Akdemir <mabeaaa@gmail.com>
2021-02-12 01:00:36 +0300

    12.02

--- Entitites/Concrete/Customer.cs
+++ Entitites/Concrete/Customer.cs
@@ -1,13 +1,14 @@
 ﻿using Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.Concrete
 {
     public class Customer:IEntity
     {
-        public int CustomerId { get; set; }
+        [Key]
         public int UserId { get; set; }
         public string CompanyName { get; set; }
     }
